9fc93361ecc19bc217d22f6e604ab04ff11ffc01
devtoprod95 <devtoprod95@users.noreply.github.com>
2026-08-07 20:37:48 +0000

    deploy: 50821003bb4d15d2645804f09bb7600c0a3e3b73

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 const cacheName = self.location.pathname
 const pages = [
 
+  "/devtoprod-hugo-blog/blog/20260808052209200/",
   "/devtoprod-hugo-blog/",
   "/devtoprod-hugo-blog/posts/",
   "/devtoprod-hugo-blog/blog/20260807185137728/",
