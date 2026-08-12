8d04cadc876a2f1073d433aa6f7710fc20b18a15
devtoprod95 <devtoprod95@users.noreply.github.com>
2026-08-12 13:02:25 +0000

    deploy: 5f9a5fdbcdc535f4a560d0ce881c872ccde69521

--- sw.js
+++ sw.js
@@ -3,6 +3,8 @@ const pages = [
 
   "/devtoprod-hugo-blog/",
   "/devtoprod-hugo-blog/posts/",
+  "/devtoprod-hugo-blog/blog/20260812214824953/",
+  "/devtoprod-hugo-blog/blog/20260812215443047/",
   "/devtoprod-hugo-blog/blog/20260812180211416/",
   "/devtoprod-hugo-blog/blog/20260812164336041/",
   "/devtoprod-hugo-blog/blog/20260812180127363/",
